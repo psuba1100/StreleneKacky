067ea03d20c039563662d42479ad0780efb474a4
psuba1100 <92215854+psuba1100@users.noreply.github.com>
2023-11-23 20:00:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="sk">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="resources/style.css"> -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap4.min.css" />
+    <link rel="stylesheet" href="resources/style.css">
+    <link rel="stylesheet" href="resources/custombootstrap.css">
+    <title>Title for this</title>
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row mb-3">
+            <h1 class="text-center display-1 title">strelené kačice</h1>
+        </div>
+        <div class="row justify-content-around">
+            <div class="col-md-6 ">
+                <div class="circle-container">
+                    <div class="circle"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 ">
+            <div class="circle-container">
+                <div class="circle"></div>
+            </div>
+        </div>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col text-center">
+            <button class="btn btn-custom-primary btn-lg">začať hru</button>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col text-center">
+            <button class="btn btn-custom-secondary btn-sm">prihlásenie</button>
+        </div>
+    </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script type=text/javascript src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
+    <script type=text/javascript src="https://cdn.datatables.net/1.13.3/js/dataTables.bootstrap4.min.js"></script>
+    <!-- <script src = "resources/script.js"></script> -->
+
+</body>
+
+</html>
